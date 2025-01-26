ada40a3d4086ad89e0ec7a63f1988adaf9b12d56
Zings <75098398+hotzings@users.noreply.github.com>
2025-01-26 14:05:10 -0500

    added OccludeVibrationSignals component

--- src/main/java/me/zings/additional_components_lib/register/ItemComponents.java
+++ src/main/java/me/zings/additional_components_lib/register/ItemComponents.java
@@ -21,6 +21,12 @@ public class ItemComponents {
             ComponentType.<Boolean>builder().codec(Codec.BOOL).build()
     );
 
+    public static final ComponentType<Boolean> OCCLUDE_VIBRATION_SIGNALS = Registry.register(
+            Registries.DATA_COMPONENT_TYPE,
+            Identifier.of(Additional_components_lib.MOD_ID, "occlude_vibration_signals"),
+            ComponentType.<Boolean>builder().codec(Codec.BOOL).build()
+    );
+
     public static void registerItemComponents(){
 
     }
