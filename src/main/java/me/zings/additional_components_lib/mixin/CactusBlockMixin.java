64de130b5fa130b7633fb02908fb147ea6cba6af
Zings <75098398+hotzings@users.noreply.github.com>
2025-06-18 16:39:12 -0400

    v 0.0.5
    Fixed cactus mixin
    added test for blast_resistance

--- src/main/java/me/zings/additional_components_lib/mixin/CactusBlockMixin.java
+++ src/main/java/me/zings/additional_components_lib/mixin/CactusBlockMixin.java
@@ -4,6 +4,7 @@ import me.zings.additional_components_lib.register.ItemComponents;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CactusBlock;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityCollisionHandler;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -15,9 +16,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(CactusBlock.class)
 public abstract class CactusBlockMixin {
 	@Inject(at = @At("HEAD"), method = "onEntityCollision", cancellable = true)
-	private void AdditionalComponentsLib$cactus_proof_items(BlockState state, World world, BlockPos pos, Entity entity, CallbackInfo ci) {
+	private void AdditionalComponentsLib$cactus_proof_items(
+			BlockState state,
+			World world,
+			BlockPos pos,
+			Entity entity,
+			EntityCollisionHandler handler,  // new parameter!
+			CallbackInfo ci
+	) {
 		if (entity instanceof ItemEntity itemEntity) {
-            if (itemEntity.getStack().getOrDefault(ItemComponents.CACTUS_RESISTANT, false)) {
+			if (itemEntity.getStack().getOrDefault(ItemComponents.CACTUS_RESISTANT, false)) {
 				ci.cancel();
 			}
 		}
